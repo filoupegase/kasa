fe0d712d2991c77f40d7bcadac83a75d0b14f3a7
filoupegase <loisoncorentin@yahoo.fr>
2021-11-21 17:28:02 +0100

    container

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo.svg";
-import "./index.scss";
+import "./header.scss";
 
 
 const Header = () => {
   return (
     <nav>
       <Link to="/">
-        <img style={{ height: 68 }} src={logo} alt="logo" />
+        <img className="header-logo" src={logo} alt="logo" />
       </Link>
       <div className="navlink">
         <Link to="/">Accueil</Link>
