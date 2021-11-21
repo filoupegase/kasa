16271cadec88489748bc58a0cc0c1f74188f942f
filoupegase <loisoncorentin@yahoo.fr>
2021-11-21 17:19:11 +0100

    container css

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import NotFound from "./pages/NotFound/NotFound";
 import Housing from "./pages/Housing/Housing";
 import AboutUs from "./pages/AboutUs/AboutUs";
 import Footer from "./_component/Footer";
+import "./assets/style/scss/_common.scss";
+
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} strict exact />
-        <Route path="/housing/:housingNumber" element={<Housing />} strict exact />
-        <Route path="/about-us" element={<AboutUs />} strict exact />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} strict exact />
+          <Route path="/housing/:housingNumber" element={<Housing />} strict exact />
+          <Route path="/about-us" element={<AboutUs />} strict exact />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
       <Footer />
     </>
   );
