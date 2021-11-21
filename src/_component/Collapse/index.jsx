1a79e27e06e46820e5c9e5305581b47b2ca2d154
filoupegase <loisoncorentin@yahoo.fr>
2021-11-21 18:43:09 +0100

    collaps

--- src/_component/Collapse/index.jsx
+++ src/_component/Collapse/index.jsx
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import "./Collapse.scss";
 
 
 class Collapse extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isOpen: false
+    };
   }
 
   render() {
     const collapseIcon = "fas fa-chevron-down";
     return (
       <>
-        <h2>
-          <span>{this.props.title}</span>
-          <span className={collapseIcon}></span>
-        </h2>
+        <div className="collapse">
+          <h2 style={{ margin: 0, fontWeight: 400 }}>
+            {this.props.title}
+          </h2>
+          <span style={{ height: 24, cursor: "pointer" }} className={collapseIcon} />
+        </div>
         <p>{this.props.textArray}</p>
       </>
     );
