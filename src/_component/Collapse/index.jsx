80fe937abe2954aabe5926b26c76342dcbc4d62b
filoupegase <loisoncorentin@yahoo.fr>
2021-11-21 19:41:52 +0100

    end components collapse

--- src/_component/Collapse/index.jsx
+++ src/_component/Collapse/index.jsx
@@ -21,13 +21,13 @@ class Collapse extends Component {
     const collapseIcon = `fas fa-chevron-${this.state.isOpen ? "up" : "down"}`;
     return (
       <>
-        <div className="collapse-header">
-          <h2 style={{ margin: 0, fontWeight: 400 }}>
+        <div className="collapse">
+          <h2 className="collapse-header">
             {this.props.title}
           </h2>
           <span onClick={this.updateIsOpen} style={{ cursor: "pointer", fontSize: 28 }} className={collapseIcon} />
         </div>
-        <div className="collapse-hidden">
+        <div className={`collapse-hidden ${this.state.isOpen ? "is-open" : "is-close"}`}>
           <p>{this.props.textArray}</p>
         </div>
       </>
