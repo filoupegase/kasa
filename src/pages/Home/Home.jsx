108b23e6ccf5ca371c1faa0a86c8dcef768b0978
filoupegase <loisoncorentin@yahoo.fr>
2021-11-23 08:50:36 +0100

    page home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,36 +1,26 @@
-import React from "react";
-import logo from "../../assets/images/logo.svg";
-import "./Home.css";
+import React, { Component } from "react";
+import "./Home.scss";
 import Banner from "../../_component/Banner";
 import ImageCoast from "../../assets/images/coast-landscape.jpeg";
 
 
-const Home = () => {
-  return (
-    <>
-      <Banner
-        imgSource={ImageCoast}
-        altText="Un paysage de cote"
-        title="Chez vous, partout et ailleurs"
-      />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/Home.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </>
-  );
-};
+class Home extends Component {
+
+  render() {
+    return (
+      <>
+        <Banner
+          imgSource={ImageCoast}
+          altText="Un paysage de cote"
+          title="Chez vous, partout et ailleurs"
+        />
+        <div className="Home">
+          <div className="App-header">
+          </div>
+        </div>
+      </>
+    );
+  };
+}
 
 export default Home;
