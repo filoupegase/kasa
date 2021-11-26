638c318e84f1c273b0c4969aea4d1c98aa15e947
filoupegase <loisoncorentin@yahoo.fr>
2021-11-26 12:01:20 +0100

    rename index.js for more rentatbility

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./pages/Header";
@@ -9,21 +9,23 @@ import "./assets/style/scss/_common.scss";
 import Footer from "./_component/Footer";
 
 
-const App = () => {
-  return (
-    <>
-      <div className="container">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} strict exact />
-          <Route path="/housing/:housingNumber" element={<Housing />} strict exact />
-          <Route path="/about-us" element={<AboutUs />} strict exact />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-      <Footer />
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} strict exact />
+            <Route path="/housing/:housingNumber" element={<Housing />} strict exact />
+            <Route path="/about-us" element={<AboutUs />} strict exact />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </>
+    );
+  };
+}
 
 export default App;
