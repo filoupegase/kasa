0c9a7b5ff47c81806f8e3de77c6cd7737f801bdd
filoupegase <loisoncorentin@yahoo.fr>
2021-11-29 13:47:47 +0100

    feat/page not found done

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./NotFound.scss";
 
 
@@ -8,7 +9,18 @@ class NotFound extends Component {
       <>
         <div className="container-not-found">
           <h1 className="big-error">404</h1>
-          <h2>Oups! La page de que vous demander n'existe pas</h2>
+          <h2
+            style={{ marginBottom: 130 }}
+          >Oups! La page de que vous demander n'existe pas</h2>
+          <div
+            style={{ marginBottom: 130 }}
+          >
+            <Link
+              className="link-to"
+              to={"/"}
+            >Retourner sur la page d'acceuil
+            </Link>
+          </div>
         </div>
       </>
     );
